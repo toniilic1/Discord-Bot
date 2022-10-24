195ac50c417e928d78557876c5f4692522409571
Toni Ilic <toniilic01@gmail.com>
2022-10-25 00:20:52 +0200

    token and guild id comment

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ import hikari
 from tokens import token_id, guild_id
 
 #must be granted bot, commands and administrator rights
+# use token id and guild id of your server for privacy
 bot = lightbulb.BotApp(
     token=token_id,
     default_enabled_guilds=(guild_id)#Register server with an ID so the commands are faster
